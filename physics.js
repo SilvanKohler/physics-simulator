568553743e602b82a8c0ba39ac94e8d819808fd9
CREEPZOMBEY <creepzombey@gmail.com>
2019-03-29 21:43:03 +0100

    improvement

--- physics.js
+++ physics.js
@@ -1,15 +1,35 @@
+var enterfull;
+var closefull;
+function preload(){
+  enterfull = loadImage('enter.png');
+  closefull = loadimage('exit.png');
+}
+
 function setup(){
-  createCanvas(window.innerWidth, window.innerHeight);
+  createCanvas(windowWidth, windowHeight);
   background(51);
-  fullscreen(true);
   ball = new object();
   frameRate(60);
   ball.run = true;
 }
+
 function draw(){
   background(51);
   ball.update();
   ball.show();
+  var full = fullscreen();
+  var notfull = !fullscreen();
+  if (!notfull) {
+    image(closefull, windowWidth - round(windowHeight * windowWidth / 65280), 0, round(windowHeight * windowWidth / 65280), round(windowHeight * windowWidth / 65280));
+  }
+  if (!full) {
+    image(enterfull, windowWidth - round(windowHeight * windowWidth / 65280), 0, round(windowHeight * windowWidth / 65280), round(windowHeight * windowWidth / 65280));
+  }
+}
+function mouseClicked(){
+  if (mouseX >= windowWidth - round(windowHeight * windowWidth / 65280) && mouseX <= windowWidth && mouseY >= 0 && mouseY <= round(windowHeight * windowWidth / 65280)){
+    fullscreen(!fullscreen());
+  }
 }
 function mouseDragged(){
   ball.run = false;
@@ -23,5 +43,5 @@ function mouseReleased(){
 }
 function windowResized() {
   ball.yvel = -1;
-  resizeCanvas(window.innerWidth, window.innerHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
