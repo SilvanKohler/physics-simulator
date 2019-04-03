c3c5b9d966a619224ee7517ed3acb52ca2ce3737
CREEPZOMBEY <creepzombey@gmail.com>
2019-04-03 13:20:16 +0200

    improvement

--- physics.js
+++ physics.js
@@ -1,46 +1,23 @@
-//let enterfull;
-//let closefull;
-
-//function preload(){
-//  enterfull = loadImage('enter.png');
-//  closefull = loadimage('exit.png');
-//}
-
 function setup(){
   createCanvas(windowWidth, windowHeight);
   background(51);
-  ball = new object();
+  ball = new Ball();
   frameRate(60);
-  ball.run = true;
 }
 
 function draw(){
   background(51);
   ball.update();
   ball.show();
-  //if (fullscreen()) {
-  //  image(closefull, windowWidth - round(windowHeight * windowWidth / 20000), 0, round(windowHeight * windowWidth / 20000), round(windowHeight * windowWidth / 20000));
-  //}
-  //if (!fullscreen()) {
-  //    image(enterfull, windowWidth - round(windowHeight * windowWidth / 20000), 0, round(windowHeight * windowWidth / 20000), round(windowHeight * windowWidth / 20000));
-  //}
-}
-function mouseClicked(){
-  //if (mouseX >= windowWidth - round(windowHeight * windowWidth / 20000) && mouseX <= windowWidth && mouseY >= 0 && mouseY <= round(windowHeight * windowWidth / 20000)){
-  //  fullscreen(!fullscreen());
-  //}
 }
 function mouseDragged(){
-  ball.run = false;
-  this.xvel = accelerationX;
-  this.yvel = accelerationY;
-  ball.x = mouseX;
-  ball.y = mouseY;
+  if ((mouseX > (ball.x - ball.mass/2) && mouseX < (ball.x + ball.mass/2)) && ((mouseY > (ball.y - ball.mass/2) && mouseY < (ball.y + ball.mass/2)))){
+    ball.dragged = true;
+  }
 }
 function mouseReleased(){
-  ball.run = true;
+  ball.dragged = false;
 }
 function windowResized() {
-  ball.yvel = -1;
   resizeCanvas(windowWidth, windowHeight);
 }
